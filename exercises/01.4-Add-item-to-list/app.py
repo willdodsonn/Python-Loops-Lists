cad333826aa1e1b93bd8f06c1d414042ad5a0482
willdodsonn <sodlax38@gmail.com>
2022-03-17 00:54:35 +0000

    first commit

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,13 @@
-#Remember import random function here:
+# Remember import random function here:
+import random
 
-my_list = [4,5,734,43,45]
+my_list = [4, 5, 734, 43, 45]
 
-#The magic is here:
+# The magic is here:
+  aux_list = []
+   randonlength = random.randint(1, 100)
+
+      for i in range(randonlength):
+           aux_list.append(randonlength)
+            i += i
+    return aux_list
